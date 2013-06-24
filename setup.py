0dff521e35d2d9bf4fe80e1eda38999521a3ae7e
Icaro Medeiros <icaro.medeiros@gmail.com>
2013-06-24 00:53:45 -0300

    trying to make setup.py to work with pygments

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
-entry_points = {
-    "pygments.lexer": [
-            "n3  = semantic_web_pygments.sw:Notation3Lexer",
-            "sparql = semantic_web_pygments.sw:SparqlLexer"
-        ],
-}
+from setuptools import setup, find_packages
+
+setup (
+    name='semantic_web_pygments',
+    packages=find_packages(),
+    entry_points = """
+[pygments.lexer]
+n3  = semantic_web_pygments.sw:Notation3Lexer
+"""
+)
